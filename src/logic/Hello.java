7ee15f31b66eff18ecc133155734ef5d7b17dda5
Jettapat <hennet2@hotmail.com>
2016-01-21 16:43:00 +0700

    Change Hello
    HelloGithub

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -15,7 +15,7 @@ public class Hello {
 
 	public static String greetingMessage(){
 		// TODO Implement this method to return string "Hello GitHub".
-		return "Hello Github";
+		return "HelloGithub";
 	}
 	
 	public static int[] bubbleSort(int[] array){
